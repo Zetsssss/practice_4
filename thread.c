e92bcff703463159120e883b1603acd66717fc25
Zetsssss <jeykeyqq@mail.ru>
2025-05-22 20:17:35 +0300

    Update thread.c

--- thread.c
+++ thread.c
@@ -3,36 +3,51 @@
 #include <unistd.h>
 #include <pthread.h>
 
-void* task(void* arg) {
-    int time = *(int*)arg;
-    sleep(time);
-    printf("Поток работал %d секунд\n", time);
+typedef struct {
+    int index;
+    int sleep_time;
+} ThreadData;
+
+void* thread_task(void* arg) {
+    ThreadData* data = (ThreadData*)arg;
+    printf("Поток %d начал работу (будет работать %d секунд)\n", data->index + 1, data->sleep_time);
+    sleep(data->sleep_time);
+    printf("Поток %d завершил работу через %d секунд\n", data->index + 1, data->sleep_time);
     free(arg);
     return NULL;
 }
 
 int main() {
-    pthread_t tid[5];
-    int times[5];
+    char choice;
+    do {
+        pthread_t threads[5];
+        int sleep_times[5];
+        
+        // Ввод времени для каждого потока
+        for (int i = 0; i < 5; i++) {
+            printf("Введите время для потока %d: ", i + 1);
+            scanf("%d", &sleep_times[i]);
+        }
+        
+        // Создание потоков
+        for (int i = 0; i < 5; i++) {
+            ThreadData* data = malloc(sizeof(ThreadData));
+            data->index = i;
+            data->sleep_time = sleep_times[i];
+            pthread_create(&threads[i], NULL, thread_task, data);
+        }
+        
+        
+        // Ожидание завершения всех потоков
+        for (int i = 0; i < 5; i++) {
+            pthread_join(threads[i], NULL);
+        }
+        
+        printf("Все потоки завершены!\n\n");
+        printf("Нажмите 'r' для повтора или 'x' для выхода: ");
+        scanf(" %c", &choice);
+        
+    } while (choice != 'x');
     
-    // Ввод времени
-    for (int i = 0; i < 5; i++) {
-        printf("Введите время для потока %d: ", i+1);
-        scanf("%d", &times[i]);
-    }
-    
-    // Создание потоков
-    for (int i = 0; i < 5; i++) {
-        int* arg = malloc(sizeof(int));
-        *arg = times[i];
-        pthread_create(&tid[i], NULL, task, arg);
-    }
-    
-    // Ожидание завершения
-    for (int i = 0; i < 5; i++) {
-        pthread_join(tid[i], NULL);
-    }
-    
-    printf("Все потоки завершены!\n");
     return 0;
 }
